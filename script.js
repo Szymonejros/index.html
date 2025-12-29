ca0250fb5aa208c1be81fff1771242e18ccc1c70
Szymonejros <szymcio0105@gmail.com>
2025-12-29 15:44:02 +0100

    Update 1.3.0

---
+++ script.js
@@ -0,0 +1,43 @@
+function showHome() {
+    document.getElementById("content").innerHTML = `
+        <h1>Strona główna</h1>
+        <p>Witaj na mojej pierwszej stronie 😄</p>
+    `;
+}
+
+function showAuthor() {
+    document.getElementById("content").innerHTML = `
+        <h1>O autorze</h1>
+        <p>Stworzone przez <b>Szymon Rak</b> &copy; 2024</p>
+        <p>Uczę się HTML i robię swoje pierwsze strony 😎</p>
+    `;
+}
+
+function showContact() {
+    document.getElementById("content").innerHTML = `
+        <h1>Kontakt</h1>
+        <a href="https://www.youtube.com/channel/UCHjeQMZe8vW_uVOaW9evdQw">Youtube</a>
+        <p>Email: NULL</p>
+        <p>Telegram: @Szymonejros </p>
+        <p>Discord: szymon02034</p>
+    `;
+}
+function showAktualizacje() {
+    document.getElementById("content").innerHTML = `
+    <h1>Aktualizacje</h1>
+    <ul>
+    <h2>
+        <li> strona została utworzona
+        <li>29.12.2025: Dodano:
+                         interaktywne Menu"
+                         Ikonę
+                         Zmieniono Tytuł
+                         Zmieniono tło</li>
+                         Dodano stronę kontaktową
+                         Poprawiono błędy w kodzie
+                         Zaktualizowano sekcję "O autorze"
+    </h2>
+    `;
+}
+// pokaż stronę główną po starcie
+showHome();
